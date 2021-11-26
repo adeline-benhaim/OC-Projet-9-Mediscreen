cd322e0ae3255f86bb2ca5be1ba8c4af7cdb9f7c
adeline-benhaim <adelinebenhaim@gmail.com>
2021-11-26 17:30:25 +0100

    Add diabetes triggers in patientReport

--- patientReport/src/main/java/com/mediscreen/patientReport/model/DiabetesTriggers.java
+++ patientReport/src/main/java/com/mediscreen/patientReport/model/DiabetesTriggers.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class DiabetesTriggers {
 
     public final static List<String> TriggersList = Arrays.asList(
-                    "Hémoglobine A1C",
+                    "Hémoglobine A1C","H&eacute;moglobine A1C",
                     "Microalbumine",
                     "Taille",
                     "Poids",
                     "Fumeur",
                     "Anormal",
-                    "Cholestérol",
+                    "Cholestérol","Cholest&eacute;rol",
                     "Vertige",
                     "Rechute",
-                    "Réaction",
+                    "Réaction","R&eacute;action",
                     "Anticorps");
 }
